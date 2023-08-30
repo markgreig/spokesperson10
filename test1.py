8ec8e946e814d1d3364a0ed574c097d0dec43e7e
markgreig <53443668+markgreig@users.noreply.github.com>
2023-08-30 13:45:07 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,54 +2,39 @@
 # coding: utf-8
 
 # In[ ]:
-
+from collections import Counter
 
 import re
 import streamlit as st
 
-st.title('My App') 
+spokesperson_data = Counter()
+data = st.text_input("Paste text here")
 
+for item in data:
+    parts = item.split('|')
+    for part in parts:
+        name, frequency = part.rsplit(' ', 1)
+        frequency = int(frequency)
+        spokesperson_data[name] += frequency
 
-import pandas as pd
+# Step 2: Create a DataFrame for the table
+df = pd.DataFrame(list(spokesperson_data.items()), columns=["Spokesperson", "Frequency"])
 
-text = st.text_input("Paste text here")
+df = df.sort_values(by='Frequency', ascending=False)
 
-# Extract rows
-rows = re.findall(r'([^\d]+ [\d\w]+ [^\d]+)\s(\d+)', text)
+# Display the DataFrame using Streamlit
+st.write(df)
 
-# Split spokesperson column 
-data = []
-for row in rows:
-  spokesperson = row[0].strip()
-  spokespeople = spokesperson.split('|')
-  spokespeople = [name.strip() for name in spokespeople]
-  frequency = int(row[1])
-  
-  for spokesperson in spokespeople:
-    data.append([spokesperson, frequency])
+# Step 3: Create the CSV table
+st.write("CSV Table:")
+st.dataframe(df)
 
-# Create dataframe  
-df = pd.DataFrame(data, columns=['Spokesperson', 'Frequency'])
-
-# Group by Spokesperson and calculate the sum of Frequency
-result = df.groupby('Spokesperson')['Frequency'].sum().reset_index()
-
-# Sort by Frequency in descending order
-result = result.sort_values(by='Frequency', ascending=False)
-
-result.to_csv('top_spokespeople.csv', index=False)
-
-
-@st.cache
-def convert_df(result):
-     return result.to_csv().encode('utf-8')
-
-csv = convert_df(result)
-
+# Step 4: Generate and download the CSV file
+csv_file = df.to_csv(index=False)
 st.download_button(
-     label="Download data as CSV",
-     data=csv,
-     file_name='top_spokespeople.csv',
-     mime='text/csv',
- )
-
+    label="Download CSV",
+    data=csv_file.encode(),
+    key="download-button",
+    file_name="spokesperson_frequency.csv",
+    mime="text/csv",
+)
