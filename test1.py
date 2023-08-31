244faec417ad2d0ab1e2d5f3485f2e3be4f93e7e
markgreig <53443668+markgreig@users.noreply.github.com>
2023-08-31 11:39:02 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,38 +2,52 @@
 # coding: utf-8
 
 # In[ ]:
+import re
 import streamlit as st
 import pandas as pd
 
-# Example data
-data = st.text_input("Paste text here")
+@st.cache
+def process_data(text):
+    # Extract rows
+    rows = re.findall(r'([^\d]+ [\d\w]+ [^\d]+)\s(\d+)', text)
 
-# Function to split and group spokespeople
-def process_data(data):
-    rows = data.split("\n")
-    processed_data = []
+    # Split spokesperson column and create data list
+    data = []
     for row in rows:
-        spokespeople = row.split("|")
-        for spokesperson in spokespeople:
-            processed_data.append([spokesperson.strip(), 1])
-    df = pd.DataFrame(processed_data, columns=["Spokesperson", "Frequency"])
-    df = df.groupby("Spokesperson").sum().reset_index()
-    return df
+        spokespersons = [name.strip() for name in row[0].strip().split('|')]
+        frequency = int(row[1])
+        for spokesperson in spokespersons:
+            data.append([spokesperson, frequency])
 
-# Streamlit app
+    # Create dataframe  
+    df = pd.DataFrame(data, columns=['Spokesperson', 'Frequency'])
+
+    # Group by Spokesperson and calculate the sum of Frequency
+    result = df.groupby('Spokesperson')['Frequency'].sum().reset_index()
+
+    # Sort by Frequency in descending order
+    result = result.sort_values(by='Frequency', ascending=False)
+
+    return result
+
 def main():
-    st.title("Spokesperson Frequency")
-    st.write("Preview of the CSV file:")
+    st.title('My App') 
 
-    # Process the data
-    df = process_data(data)
+    text = st.text_input("Paste text here")
 
-    # Show the preview table
-    st.write(df)
+    if text:
+        result = process_data(text)
 
-    # Download link for the CSV file
-    csv = df.to_csv(index=False)
-    st.download_button("Download CSV", data=csv, file_name="spokesperson_frequency.csv")
+        st.write(result)
+
+        csv = result.to_csv(index=False).encode('utf-8')
+
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='top_spokespeople.csv',
+            mime='text/csv'
+        )
 
 if __name__ == "__main__":
     main()
