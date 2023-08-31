73558ea3399401f3f250f657a9099ad32401d3eb
markgreig <53443668+markgreig@users.noreply.github.com>
2023-08-31 12:08:42 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,10 +3,10 @@
 
 # In[ ]:
 import io
-import streamlit as st
+import streamlit as st 
 import pandas as pd
 
-st.title("Generate Spokesperson Frequency CSV") 
+st.title("Generate Spokesperson Frequency CSV")
 
 # Get text input
 text = st.text_input("Paste data here")
@@ -17,23 +17,26 @@ if not text:
 else:
     # Convert text to DataFrame
     try:
-        df = pd.read_csv(io.StringIO(text)) 
+        df = pd.read_csv(io.StringIO(text))
     except Exception as e:
         st.error("Error parsing input data")
 
     # Split rows on '|'
     df = df.apply(lambda x: x.str.split('|').explode()).reset_index(drop=True)
-    
-    # Check number of columns
-    if len(df.columns) == 1:
-        # Use unnamed column
-        df = df[df.columns[0]] 
+
+    # Check if df is Series or DataFrame
+    if isinstance(df, pd.Series):
+        # Series has 1 unnamed column  
+        df = df.to_frame() 
     else:
-        # Rename column to 'Spokesperson'
-        df = df.rename(columns={df.columns[0]: 'Spokesperson'})
-        
-    # Group and count frequencies
-    df = df.groupby(df.columns[0], as_index=False)[df.columns[0]].count().rename(columns={df.columns[0]: "Frequency"})
+        # DataFrame has columns
+        pass
+    
+    # Rename column to 'Spokesperson'
+    df = df.rename(columns={df.columns[0]: 'Spokesperson'})
+
+    # Group and count frequencies 
+    df = df.groupby("Spokesperson", as_index=False)["Spokesperson"].count().rename(columns={"Spokesperson": "Frequency"})
 
     # Display preview
     st.dataframe(df)
@@ -44,11 +47,11 @@ else:
         return df.to_csv().encode('utf-8')
 
     csv = convert_df(df)
-    
+
     st.download_button(
-       "Press to Download",
-       csv,
-       "spokesperson_frequency.csv",
-       "text/csv",
-       key='download-csv'
+        "Press to Download",
+        csv,
+        "spokesperson_frequency.csv",
+        "text/csv",
+        key='download-csv'
     )
