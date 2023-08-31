a6741748559c81270797aab609788b46652fe475
markgreig <53443668+markgreig@users.noreply.github.com>
2023-08-31 12:06:37 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -11,33 +11,40 @@ st.title("Generate Spokesperson Frequency CSV")
 # Get text input
 text = st.text_input("Paste data here")
 
-# Convert text to DataFrame
-df = pd.read_csv(io.StringIO(text))
+# Validate input 
+if not text:
+    st.error("Please enter valid CSV data")
+else:
+    # Convert text to DataFrame
+    try:
+        df = pd.read_csv(io.StringIO(text))
+    except Exception as e:
+        st.error("Error parsing input data")
 
-# Split all rows on '|'  
-df = df.apply(lambda x: x.str.split('|').explode()).reset_index(drop=True)
+    # Split all rows on '|' 
+    df = df.apply(lambda x: x.str.split('|').explode()).reset_index(drop=True)
 
-# Keep only 'Spokesperson' column
-df = df['Spokesperson']  
+    # Keep only 'Spokesperson' column
+    df = df['Spokesperson']
 
-# Group by spokesperson and count frequencies
-df = df.groupby(df.columns[0], as_index=False)[df.columns[0]].count().rename(columns={df.columns[0]: "Frequency"})
+    # Group by spokesperson and count frequencies
+    df = df.groupby(df.columns[0], as_index=False)[df.columns[0]].count().rename(columns={df.columns[0]: "Frequency"})
 
-# Display dataframe preview
-st.dataframe(df)
+    # Display dataframe preview
+    st.dataframe(df)
 
-# Download CSV file 
-@st.cache
-def convert_df(df):
-    return df.to_csv().encode('utf-8')
+    # Download CSV file
+    @st.cache
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')
 
-csv = convert_df(df)
+    csv = convert_df(df)
 
-st.download_button(
-    "Press to Download",
-    csv,
-    "spokesperson_frequency.csv",
-    "text/csv",
-    key='download-csv'
-)
+    st.download_button(
+        "Press to Download",
+        csv,
+        "spokesperson_frequency.csv", 
+        "text/csv",
+        key='download-csv'
+    )
 
