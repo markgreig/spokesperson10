a993fd5bc3734a7d414b83d25b32f2084827781c
markgreig <53443668+markgreig@users.noreply.github.com>
2023-08-31 12:07:46 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,45 +6,49 @@ import io
 import streamlit as st
 import pandas as pd
 
-st.title("Generate Spokesperson Frequency CSV")
+st.title("Generate Spokesperson Frequency CSV") 
 
 # Get text input
 text = st.text_input("Paste data here")
 
-# Validate input 
+# Validate input
 if not text:
     st.error("Please enter valid CSV data")
 else:
     # Convert text to DataFrame
     try:
-        df = pd.read_csv(io.StringIO(text))
+        df = pd.read_csv(io.StringIO(text)) 
     except Exception as e:
         st.error("Error parsing input data")
 
-    # Split all rows on '|' 
+    # Split rows on '|'
     df = df.apply(lambda x: x.str.split('|').explode()).reset_index(drop=True)
-
-    # Keep only 'Spokesperson' column
-    df = df['Spokesperson']
-
-    # Group by spokesperson and count frequencies
+    
+    # Check number of columns
+    if len(df.columns) == 1:
+        # Use unnamed column
+        df = df[df.columns[0]] 
+    else:
+        # Rename column to 'Spokesperson'
+        df = df.rename(columns={df.columns[0]: 'Spokesperson'})
+        
+    # Group and count frequencies
     df = df.groupby(df.columns[0], as_index=False)[df.columns[0]].count().rename(columns={df.columns[0]: "Frequency"})
 
-    # Display dataframe preview
+    # Display preview
     st.dataframe(df)
 
-    # Download CSV file
+    # Download CSV
     @st.cache
     def convert_df(df):
         return df.to_csv().encode('utf-8')
 
     csv = convert_df(df)
-
+    
     st.download_button(
-        "Press to Download",
-        csv,
-        "spokesperson_frequency.csv", 
-        "text/csv",
-        key='download-csv'
+       "Press to Download",
+       csv,
+       "spokesperson_frequency.csv",
+       "text/csv",
+       key='download-csv'
     )
-
