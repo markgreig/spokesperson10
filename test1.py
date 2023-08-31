4354518fb052b8ce00f7d419b76b742373bb8a5a
markgreig <53443668+markgreig@users.noreply.github.com>
2023-08-31 12:04:18 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,52 +2,41 @@
 # coding: utf-8
 
 # In[ ]:
-import re
 import streamlit as st
-
-st.title('My App') 
-
-
 import pandas as pd
 
-text = st.text_input("Paste text here")
+st.title("Generate Spokesperson Frequency CSV")
 
-# Extract rows
-rows = re.findall(r'([^\d]+ [\d\w]+ [^\d]+)\s(\d+)', text)
+# Get text input
+text = st.text_input("Paste data here") 
 
-# Split spokesperson column 
-data = []
-for row in rows:
-  spokesperson = row[0].strip()
-  spokespeople = spokesperson.split('|')
-  spokespeople = [name.strip() for name in spokespeople]
-  frequency = int(row[1])
-  
-  for spokesperson in spokespeople:
-    data.append([spokesperson, frequency])
+# Load data from text input
+df = pd.read_csv(text)
 
-# Create dataframe  
-df = pd.DataFrame(data, columns=['Spokesperson', 'Frequency'])
+# Split all rows on '|' 
+df = df.apply(lambda x: x.str.split('|').explode()).reset_index(drop=True)
 
-# Group by Spokesperson and calculate the sum of Frequency
-result = df.groupby('Spokesperson')['Frequency'].sum().reset_index()
+# Keep only 'Spokesperson' column
+df = df['Spokesperson']
 
-# Sort by Frequency in descending order
-result = result.sort_values(by='Frequency', ascending=False)
+# Group by spokesperson and count frequencies
+df = df.groupby(df.columns[0], as_index=False)[df.columns[0]].count().rename(columns={df.columns[0]: "Frequency"})
 
-result.to_csv('top_spokespeople.csv', index=False)
+# Display dataframe preview 
+st.dataframe(df)
 
+# Download CSV file
+@st.cache  
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
 
-@st.cache
-def convert_df(result):
-     return result.to_csv().encode('utf-8')
-
-csv = convert_df(result)
+csv = convert_df(df)
 
 st.download_button(
-     label="Download data as CSV",
-     data=csv,
-     file_name='top_spokespeople.csv',
-     mime='text/csv',
- )
+    "Press to Download",
+    csv, 
+    "spokesperson_frequency.csv",
+    "text/csv",
+    key='download-csv'
+)
 
